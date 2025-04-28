355e6eb111b5f273679d3dc64f1d17d7ab9de57e
Leeyujin-02 <dbwlsl9440@gmail.com>
2025-04-28 13:41:15 +0900

    🔧 이미지 링크 수정
    질문 수정에 따른 적합한 이미지로 교체

--- app/services/images.py
+++ app/services/images.py
@@ -1,16 +1,13 @@
 # 질문별 이미지
 
 image_links = {
-    "아침": [
-        "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FvzT6w%2FbtsJn8MQstW%2FyTiD5bxJP0RWUKuEcC25C1%2Fimg.png",
-        "https://cdn.maily.so/lbhy0ocngx0lafwrd8ii3kanjbi9"
-    ],
-    "라면": [
-        "https://postfiles.pstatic.net/MjAxODAxMjVfMTMw/MDAxNTE2ODUzMzE4MTc4.n9PaO3z0CQrasgC126-6SOsZlMYVGu2zi_7LW-BHackg.DobDu4vtOVNIvNHAwb85dAuy-jH0Gq8MyNr35fyYPIMg.JPEG.qe4865/4.jpg?type=w580",
-        "https://cdnweb01.wikitree.co.kr/webdata/editor/201810/22/img_20181022210429_cfbf09cc.jpg"
-    ],
-    "외계인": [
-        "https://www.jjalbang.today/files/jjalbox/2023/08/20230826_64e9d76eaeada.jpeg",
-        "https://mblogthumb-phinf.pstatic.net/MjAyMDEwMTVfMjk2/MDAxNjAyNzQ1MDc0NDky.mx3soobsJaMJor0tFKNE-osV6u21mfe0wwhomoU5aoIg.mnRnG4zdMYd6MxcOvBJBR45urXU6UqmoisxApYB1Ohwg.JPEG.lumos_y/1602745073935.jpg?type=w800"
+    "link": [
+        "https://i.pinimg.com/736x/cc/f5/69/ccf569dde2958006e08aa4fcba49939d.jpg",
+        "https://www.jjalbang.today/files/jjalbox/2022/02/20220224_6217405f7b27f.jpg",
+        "https://mblogthumb-phinf.pstatic.net/MjAyMDEwMTVfMjk2/MDAxNjAyNzQ1MDc0NDky.mx3soobsJaMJor0tFKNE-osV6u21mfe0wwhomoU5aoIg.mnRnG4zdMYd6MxcOvBJBR45urXU6UqmoisxApYB1Ohwg.JPEG.lumos_y/1602745073935.jpg?type=w800",
+        "https://i.pinimg.com/736x/de/f5/a2/def5a28fc665006dfe81098d0678bfd8.jpg",
+        "https://mblogthumb-phinf.pstatic.net/MjAyMTA4MjFfMTAx/MDAxNjI5NTU3MTUzNzg4.mXEv2psD93O8aqiXm6gQO8Ys4p6r-KuqiDR39QRMqXEg.3cLE2fmn2bAPlgn9qXWdTO6Q6D3apApHVFphbeHYMLUg.JPEG.chooddingg/6EDC7128-CAA9-4EDE-9AE9-71D77D4D300F-16837-0000081DCC181025_file.jpg?type=w800",
+        "https://static.hubzum.zumst.com/hubzum/2019/03/05/11/b4e02040204e4f17aef43b13a8d7ec30.jpg",
+        "https://cdn.maily.so/sniatc1l0sypw9gdvacahb1x9zf7"
     ]
 }
