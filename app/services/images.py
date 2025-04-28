350afc43da5abde79b821ad20319a14895c2483f
Leeyujin-02 <dbwlsl9440@gmail.com>
2025-04-28 13:08:06 +0900

    ✨ 질문별 이미지 링크 추가
    질문별 이미지 링크 추가

---
+++ app/services/images.py
@@ -0,0 +1,16 @@
+# 질문별 이미지
+
+image_links = {
+    "아침": [
+        "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FvzT6w%2FbtsJn8MQstW%2FyTiD5bxJP0RWUKuEcC25C1%2Fimg.png",
+        "https://cdn.maily.so/lbhy0ocngx0lafwrd8ii3kanjbi9"
+    ],
+    "라면": [
+        "https://postfiles.pstatic.net/MjAxODAxMjVfMTMw/MDAxNTE2ODUzMzE4MTc4.n9PaO3z0CQrasgC126-6SOsZlMYVGu2zi_7LW-BHackg.DobDu4vtOVNIvNHAwb85dAuy-jH0Gq8MyNr35fyYPIMg.JPEG.qe4865/4.jpg?type=w580",
+        "https://cdnweb01.wikitree.co.kr/webdata/editor/201810/22/img_20181022210429_cfbf09cc.jpg"
+    ],
+    "외계인": [
+        "https://www.jjalbang.today/files/jjalbox/2023/08/20230826_64e9d76eaeada.jpeg",
+        "https://mblogthumb-phinf.pstatic.net/MjAyMDEwMTVfMjk2/MDAxNjAyNzQ1MDc0NDky.mx3soobsJaMJor0tFKNE-osV6u21mfe0wwhomoU5aoIg.mnRnG4zdMYd6MxcOvBJBR45urXU6UqmoisxApYB1Ohwg.JPEG.lumos_y/1602745073935.jpg?type=w800"
+    ]
+}
